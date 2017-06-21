e96d59b0deae9c91b161c6dbbe5e609b3c7bd15f
Kfrable <kcfrable@gmail.com>
2017-06-21 12:21:29 -0400

    added photo of me

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ function About(){
 		
 			<h1>Kyle Frable</h1>
 			<h2>Web Developer</h2>
-			<Image id='me' src="./Images/me.jpg" responsive />
+			<Image id='me' src="./Images/me.png" responsive />
 			<p className='w3-container w3-center w3-animate-right'>Being that I come from multiple industries that required me to be  quick on my feet, being adaptable is something that set me apart. In being able to integrate myself into situations, doesn’t mean I compromise my values. I stand by what i’ve been taught, to always be kind and happy, and treat others with respect ,and show that i’m willing to work.</p>
 
 		</div>
