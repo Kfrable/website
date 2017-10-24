b3c29145b39b31537806c93d3377fa9a31a5585c
Kfrable <kcfrable@gmail.com>
2017-10-24 18:19:27 -0400

    adding descriptions to projects, cleaning up the styling

--- src/components/About.js
+++ src/components/About.js
@@ -12,9 +12,36 @@ let style ={
 }
 
 let style2 ={
-	    height: '655'
+	    height: '655',
+	    'margin-top': '75'
 	        
 }
+
+// let controller = new ScrollMagic.Controller();
+// var tween = TweenMax.from("#animate", 0.5, {autoAlpha: 0, scale: 0.7});
+// var scene = new ScrollMagic.Scene({triggerElement: "a#top", duration: 200, triggerHook: "onLeave"})
+// 					.setTween(tween)
+// 					.addIndicators() 
+// 					.addTo(controller);
+// controller.scrollTo(function (newpos) {
+// 		TweenMax.to(window, 0.5, {scrollTo: {y: newpos}});
+// 	});
+
+// document.addEventListener("click", "a[href^='#']", function (e) {
+// 		var id = this.attribute("href");
+// 		if ($(id).length > 0) {
+// 			e.preventDefault();
+
+// 			// trigger scroll
+// 			controller.scrollTo(id);
+
+// 				// if supported by the browser we can even update the URL.
+// 			if (window.history && window.history.pushState) {
+// 				history.pushState("", document.title, id);
+// 			}
+// 		}
+// 	});
+
 
 function About(){
 	return(
